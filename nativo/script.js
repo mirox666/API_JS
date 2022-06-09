1bbb656db65cfe78fea663bcf59ae9b76971e1c9
Matheus Raposo <123lampiao10@gmail.com>
2022-06-09 20:51:31 -0300

    Implementando visualização da api no HTML

--- nativo/script.js
+++ nativo/script.js
@@ -1,3 +1,7 @@
+let lista = document.querySelector("#lista")
+let tabela = document.querySelector("#tabela")
+
+
 async function carregarDados(){
     const url = "https://swapi.dev/api/people/?format=json"
     try {
@@ -6,10 +10,24 @@ async function carregarDados(){
         const dados = await resultado.json()
 
         for(elementos of dados.results){
-            console.log(` Olá,sou ${elementos.name} nasci no ano  ${elementos.birth_year}`)
+            //console.log(` Olá,sou ${elementos.name} nasci no ano  ${elementos.birth_year}`)
+            //Criando elementos html dinamicamente
+            /*
+            const itemLista = document.createElement("li")
+            itemLista.classList.add("list-group-item")
+            itemLista.textContent = `olá meu nome é ${elementos.name} ,meu pesso é ${elementos.mass} , minha cor de cabelo é ${elementos.hair_color} e meu gênero é ${elementos.gender} `
+            
+            lista.appendChild(itemLista)
+            */
+            const itemTabela = document.createElement("tr")
+            const tdNome = document.createElement("td")
+            const tdPeso = document.createElement("td")
+            const tdCorOlhos = document.createElement("td")
+
+            
         }
 
-        //console.log(dados.results)
+        console.log(dados.results)
     } catch (error) {
         console.log("O seguinte erro aconteceu", error)
     }
