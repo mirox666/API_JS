376decccc25d6d108fe0658bf696e0b5aca1a89e
Matheus Raposo <123lampiao10@gmail.com>
2022-06-13 20:58:02 -0300

    Continuando api nativo com JS

--- nativo/script.js
+++ nativo/script.js
@@ -1,6 +1,7 @@
 let lista = document.querySelector("#lista")
 let tabela = document.querySelector("#tabela")
-
+let campoBusca =document.querySelector("#campo")
+let btnFiltrar = document.querySelector("#filtrar")
 
 async function carregarDados(){
     const url = "https://swapi.dev/api/people/?format=json"
@@ -19,12 +20,22 @@ async function carregarDados(){
             
             lista.appendChild(itemLista)
             */
+           //criando elementos html
             const itemTabela = document.createElement("tr")
             const tdNome = document.createElement("td")
             const tdPeso = document.createElement("td")
             const tdCorOlhos = document.createElement("td")
-
             
+            //Criando os conteúdo das colunas
+            tdNome.textContent = elementos.name
+            tdPeso.textContent = elementos.mass
+            tdCorOlhos.textContent = elementos.eye_color 
+            
+            //Adicionando os elementos em suas respectivas tags Mãe ou Pai
+            itemTabela.appendChild(tdNome)
+            itemTabela.appendChild(tdPeso)
+            itemTabela.appendChild(tdCorOlhos)
+            tabela.appendChild(itemTabela)
         }
 
         console.log(dados.results)
@@ -36,4 +47,26 @@ async function carregarDados(){
    
 }
 
-carregarDados()
+async function filtrarDados(idPersonagem){
+    const url = `https://swapi.dev/api/people/${idPersonagem}/?format=json`
+    
+    try {
+        let resultado = await fetch (url)
+        const dados = await resultado.json()
+        console.log(dados)
+
+    } catch (error) {
+        console.log("O erro é o seguinte:",erro)
+    }
+}
+
+//carregarDados()
+btnFiltrar.addEventListener('click', (evento)=>{
+    evento.preventDefault()
+    if(campoBusca.value != "" && campoBusca.value >=1 && campoBusca.value <=82){
+        filtrarDados(campoBusca.value)
+    }
+    else{
+        alert("Insira um valor no campo para pesquisar ")
+    }
+})
